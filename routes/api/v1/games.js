2ae94fe3bb50567b28d3b93e59a0505563aca4a2
Sarah Tatro <statrox4@gmail.com>
2019-09-26 17:51:40 -0600

    Set up PUT and DELETE routes to update or destroy a game

--- routes/api/v1/games.js
+++ routes/api/v1/games.js
@@ -50,4 +50,47 @@ router.post("/", function(req, res, next) {
     });
 });
 
+/*PUT single game*/
+router.put("/:id", function(req, res, next) {
+  Game.update(
+    {
+      title: req.body.title,
+      price: req.body.price,
+      releaseYear: req.body.releaseYear,
+      active: req.body.active
+    },
+    {
+      returning: true,
+      where: {
+        id: parseInt(req.params.id)
+      }
+    }
+  )
+  .then(game => {
+    res.SetHeader("Content-Type", "application/json");
+    res.status(202).send(JSON.stringify(game));
+  })
+  .catch(error => {
+    res.setHeader("Content-Type", "application/json");
+    res.status(500).send({ error });
+  });
+});
+
+/*DELETE game*/
+router.delete("/:id", function(req, res, next) {
+  Game.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(game => {
+    res.SetHeader("Content-Type", "application/json");
+    res.status(202).send(JSON.stringify(game));
+  })
+  .catch(error => {
+    res.setHeader("Content-Type", "application/json");
+    res.status(500).send({ error });
+  });
+});
+
 module.exports = router;
